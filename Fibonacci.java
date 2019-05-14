5a877606b338642d5c4e16afb0c6c4b6aa1598b3
Bismythium <33958789+Bismythium@users.noreply.github.com>
2019-05-14 14:33:04 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+//JAVA FIBONACCI PROGRAM, TAKES ARGUMENT FROM CMD
+
+public class Fibonacci {
+
+    public static void main(String args[]) {
+
+        int n = Integer.parseInt(args[0]), t1 = 0, t2 = 1;
+        
+        System.out.print("Upto " + n + ": ");
+        while (t1 <= n)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
